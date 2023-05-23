ac4658b031d2f58c9ffddd1e5643036c3bf666da
ardian56 <56733229+ardian56@users.noreply.github.com>
2023-05-23 18:32:44 +0700

    Add files via upload

---
+++ Pertemuan 9/pangkat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int pangkat(int a, int b) {
+  if (b == 0) {
+    return 1;
+  } else {
+    return a * pangkat(a,b - 1);
+  }
+}
+
+int main() {
+  int x,y;
+  cout<<"masukan bilangan : ";
+  cin>>x;
+  cout<<"masukan bilangan pangkat: ";
+  cin>>y;
+
+  int hasil = pangkat(x,y);
+  cout << "Hasil pangkat: " << hasil << endl;
+
+  return 0;
+}
+
